d55d3144d164e6080b9d1539b29a0354eb654887
utkarsh <utkarshkumar.uttu@gmail.com>
2024-01-23 04:52:58 +0530

    file_addition

--- dict.py
+++ dict.py
@@ -1,9 +1,13 @@
-list = [{'cool':'spoon', 'rupture': 'algo'},{'cool':'splash','rupture':'control'}]
+# list = [{'cool':'spoon', 'rupture': 'algo'},{'cool':'splash','rupture':'control'}]
 
-def iterate(co):
-    for i in list:
-        for key,value in enumerate(i):
-            key = i[value]
-            print(key)
+# def iterate(co):
+#     for i in list:
+#         for key,value in enumerate(i):
+#             key = i[value]
+#             print(key)
 
-iterate(list)
+# iterate(list)
+
+with open('data_file.py', 'w') as file:
+    data = input("Enter data to store: ")
+    file.write(f'data = "{data}"')
